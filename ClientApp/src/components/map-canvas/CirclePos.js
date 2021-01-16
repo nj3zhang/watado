73001e7672fa34ac9dccabdfd44a8c92d23f6e1f
Jeff Ma <jeff-2c@hotmail.com>
2021-01-16 15:37:19 -0500

    Add finishing touches

--- ClientApp/src/components/map-canvas/CirclePos.js
+++ ClientApp/src/components/map-canvas/CirclePos.js
@@ -2,15 +2,18 @@ import React, {useRef, useEffect} from 'react';
 import Circle from './Dotted_Circle.svg';
 import './CirclePos.scss';
 
-const CirclePos = ({left, top, playerData, num}) => {
-    // TODO: Add check to see where other players are at here!
-    const playerCurrPos = num == playerData.tasksDone;
-    
+const CirclePos = ({left, top, players, num}) => {
+    // TODO: Fix positions of other players if you have time. 
+    const playerNames = players.map((player) => {
+        return player.name + (player.currPlayer ? " (You)" : "");
+    }).join(' + ');
+
     return (
         <div className="circle-pos"
             style={{backgroundImage: `url(${Circle})`, left: `${left}px`, top: `${top}px`}}
         >
-            <div className="name-section">{playerCurrPos && playerData.name}</div>
+            {/* <div className="filler"></div> */}
+            <div className="name-section">{playerNames}</div>
             
         </div>
     );
--- ClientApp/src/components/map-canvas/MapCanvas.js
+++ ClientApp/src/components/map-canvas/MapCanvas.js
@@ -3,6 +3,16 @@ import './MapCanvas.scss';
 import Map from './tado_path.svg';
 import CirclePos from "./CirclePos";
 
+
+const hardPos = [
+    {left: 120, top: 620},
+    {left: 510, top: 550},
+    {left: 350, top: 310},
+    {left: 800, top: 380},
+    {left: 760, top: 110},
+    {left: 1110, top: 110},
+]
+
 // TODO: Pass in props such as player data etc.
 const MapCanvas = ({playersData}) => {
     // Update size of map based on browser window size.
@@ -21,12 +31,20 @@ const MapCanvas = ({playersData}) => {
     return (
         <div ref={mapRef} className="map-canvas" style={{backgroundImage: `url(${Map})`}}>
             {/* Hardcoded cuz y not */}
-            <CirclePos left={150} top={620} playerData={playerData} num={1} />
+            {hardPos.map((pos, i) => {
+                // Only render players in current position onto circles:
+                const matchedPlayers = playersData.filter((data) => data.tasksDone === i+1);
+                return (
+                    <CirclePos key={i} left={pos.left} top={pos.top} players={matchedPlayers} num={i+1} />
+                );
+            })}
+
+            {/* <CirclePos left={150} top={620} playerData={playerData} num={1} />
             <CirclePos left={510} top={550} playerData={playerData} num={2} />
             <CirclePos left={350} top={260} playerData={playerData} num={3} />
             <CirclePos left={780} top={390} playerData={playerData} num={4} />
             <CirclePos left={760} top={70} playerData={playerData}  num={5} />
-            <CirclePos left={1130} top={60} playerData={playerData} num={6} />
+            <CirclePos left={1130} top={60} playerData={playerData} num={6} /> */}
         </div>
     );
 }
