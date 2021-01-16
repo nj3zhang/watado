123c5f67ed0c589f4190c0e5d93c600520a1f381
Jeff Ma <jeff-2c@hotmail.com>
2021-01-16 00:03:22 -0500

    Add routes for login and signup, as well as dummy components

---
+++ ClientApp/src/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            Hello world!
+        </div>
+    );
+}
+
+export default SignUp;
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
