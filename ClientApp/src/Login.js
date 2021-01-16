9aeb7ee0fd69b5eedd588585fe2d0c4c314fa1c4
Caleb Chacko <59673175+CalebChacko@users.noreply.github.com>
2021-01-16 10:07:18 -0600

    Initial UI for Login Page

--- ClientApp/src/Login.js
+++ ClientApp/src/Login.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import './Login.scss';
+import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
 
-const Login = () => {
+import Ghost from'./assets/Pacman_Ghost_Orange.png'
+
+export const Login = () => {
     return (
-        <div>
-            Hello world!
+        <>
+        <h1>Welcome to Tado!</h1>
+        <div className="top">
+            
+            <img src={Ghost}></img>
+            <div className= "verticalLine">
+                <div className= "userInput">
+                    <TextField
+                        id="standard-read-only-input"
+                        label="Read Only"
+                        defaultValue="Hello World"
+                        InputProps={{
+                        readOnly: true,
+                    }}/> 
+                    
+                    <TextField style={{marginTop: "100px"}}
+                        id="standard-read-only-input"
+                        label="Read Only"
+                        defaultValue="Test 2"
+                        InputProps={{
+                    }}/> 
+                </div>
+            </div>
+            
         </div>
+        </>
     );
 }
 
 export default Login;
+
