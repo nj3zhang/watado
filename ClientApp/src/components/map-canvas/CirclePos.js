85bf7035b7af49b7b5f5d0eb71556db689cf2c5a
Jeff Ma <jeff-2c@hotmail.com>
2021-01-16 15:10:10 -0500

    Improve on map design and add test data for players

--- ClientApp/src/components/map-canvas/CirclePos.js
+++ ClientApp/src/components/map-canvas/CirclePos.js
@@ -2,23 +2,16 @@ import React, {useRef, useEffect} from 'react';
 import Circle from './Dotted_Circle.svg';
 import './CirclePos.scss';
 
-const CirclePos = ({left, top, num}) => {
-    // const circleRef = useRef(null);
-    // useEffect(() => {
-    //     circleRef.current.style.backgroundImage = `url(${Circle})`;
-    //     circleRef.current.style.left = stylesRef[num];
-    //     circleRef.current.style.top = stylesRef[num];
-    
-        
-        
-    // }, []);
+const CirclePos = ({left, top, playerData, num}) => {
+    // TODO: Add check to see where other players are at here!
+    const playerCurrPos = num == playerData.tasksDone;
     
     return (
         <div className="circle-pos"
-            // ref={circleRef}
             style={{backgroundImage: `url(${Circle})`, left: `${left}px`, top: `${top}px`}}
         >
-            {num}
+            <div className="name-section">{playerCurrPos && playerData.name}</div>
+            
         </div>
     );
 }
