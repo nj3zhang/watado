401f54118b173ed151e0510f9a2884c1cc066e5d
nicole <nicolezhang83@icloud.com>
2021-01-16 21:14:53 -0500

    Added hover animation

--- ClientApp/src/SignUp.js
+++ ClientApp/src/SignUp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BlobIconPNG from './components/blob.png';
 import './SignUp.scss';
+import Button from '@material-ui/core/Button';
 
 export default function SignUp () {
     return (
