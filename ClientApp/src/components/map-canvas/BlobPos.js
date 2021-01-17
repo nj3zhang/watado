5711d00e2b0d8970e3344adef3fe2d637a45a7ed
Anda Achimescu <anda.a@live.ca>
2021-01-17 01:31:55 -0500

    NEED HELP ON BLOB POS, Added all blob colors, added in colors in json file

---
+++ ClientApp/src/components/map-canvas/BlobPos.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './CirclePos.scss';
+import './BlobPos.scss';
+import BlueBlob from '../photos/Blue_Blob.png';
+import RedBlob from '../photos/Red_Blob.png';
+import YellowBlob from '../photos/Yellow_Blob.png';
+import GreenBlob from '../photos/Green_Blob.png';
+
+
+const BlobPos = ({left, top, player, num}) => {
+        if (player == null)
+            return 0;
+
+        var blobType = player.id == 1 ? "mainBlob-pos" : "leftBlob-pos";
+        console.log("color " + player.color + " name "  +player.username + " num " + num);
+        if (player.color = "blue"){
+            return (
+                <div className="mainBlob-pos" style={{left: `${left}px`, top: `${top-50}px`}}>
+                    <img src={BlueBlob} width='200px' height='190px'/>
+                </div>
+            );
+        
+        } else if (player.color = "red"){
+            return (
+                <div className="leftBlob-pos" style={{left: `${left}px`, top: `${top-50}px`}}>
+                    <img src={RedBlob} width='200px' height='190px'/>
+                </div>
+            );
+        
+        } else if (player.color = "green"){
+            return (
+                <div className="leftBlob-pos" style={{left: `${left}px`, top: `${top-50}px`}}>
+                    <img src={GreenBlob} width='200px' height='190px'/>
+                </div>
+            );
+        } else {
+            return (
+                <div className="leftBlob-pos" style={{left: `${left}px`, top: `${top-50}px`}}>
+                    <img src={YellowBlob} width='200px' height='190px'/>
+                </div>
+            );
+        }
+    
+    
+
+}
+
+export default BlobPos;
